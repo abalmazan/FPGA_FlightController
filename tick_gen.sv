1c6304071e849dc2a6b0555740b7ca9233c9b65f
abalmazan <abriahm_almazan1@baylor.edu>
2025-12-16 23:48:40 -0600

    tick_gen.sv

---
+++ tick_gen.sv
@@ -0,0 +1,22 @@
+module tick_gen (
+    input  logic clk,
+    input  logic rst,
+    output logic tick
+);
+    logic [16:0] count;
+
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            count <= 0;
+            tick  <= 1'b0;
+        end else begin
+            if (count == 17'd99999) begin
+                count <= 0;
+                tick  <= 1'b1;
+            end else begin
+                count <= count + 1;
+                tick  <= 1'b0;
+            end
+        end
+    end
+endmodule
